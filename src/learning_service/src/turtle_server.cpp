32d8a99feaef3f9ffab42bff50808203fcdae7fa
Zhilong Li <lzl12051@users.noreply.github.com>
2020-06-13 17:05:03 -0700

    turtle server

--- src/learning_service/src/turtle_server.cpp
+++ src/learning_service/src/turtle_server.cpp
@@ -1,9 +1,40 @@
-#include<ros/ros.h>
-#include<std_srvs/Trigger.h>
+#include <ros/ros.h>
+#include <std_srvs/Trigger.h>
+#include <geometry_msgs/Twist.h>
 
-int main(int argc, char** argv)
+ros::Publisher turtle_vel_pub;
+bool pubCommand = false;
+
+bool serverCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
 {
-    ros::init(argc, argv, "tuttle server");
+    pubCommand = !pubCommand;
+    ROS_INFO("Publish turtle velocity command [%s]", pubCommand == true ? "Yes" : "No");
+    res.success = true;
+    res.message = "Change turtle command state";
+
+    return true;
+}
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "turtle_server");
     ros::NodeHandle nh;
-    
+    ros::ServiceServer server = nh.advertiseService("/turtle_command", serverCallback);
+    turtle_vel_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
+
+    ROS_INFO("Ready to receive turtle command.");
+
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+        if (pubCommand)
+        {
+            geometry_msgs::Twist vel;
+            vel.angular.x = 0.2;
+            vel.linear.z = 0.5;
+            turtle_vel_pub.publish(vel);
+        }
+        loop_rate.sleep();
+    }
+    return 0;
 }
